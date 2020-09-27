be9282fe2be5a6f05844abd59325c34b56da619b
Trần Bảo Toàn <49049899+tbtoanit@users.noreply.github.com>
2020-09-27 17:06:42 +0700

    The first upload
    The first upload

---
+++ buoi8/DemoFunction.py
@@ -0,0 +1,9 @@
+danh_sach = [1,2,3,4,5,6,7,8,9,10]
+
+danh_sach_chan = list(filter(lambda a:a%2==0, danh_sach))
+
+print(danh_sach_chan)
+
+danh_sach_nhan_doi = list(map(lambda a:a*2,danh_sach))
+
+print(danh_sach_nhan_doi)
